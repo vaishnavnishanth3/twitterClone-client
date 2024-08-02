be7021bd7d0fa15e00dd665525b2fea248ed0e79
Vaishnav Nishanth A V <vaishnavnishanth3@gmail.com>
2024-08-02 16:19:35 +0530

    Update Route files and path

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv"
 import { v2 as cloudinary} from "cloudinary"
 
 import dbConnection from "./db/connectMongoDB.js"
-import authRoutes from "./routes/auth.routes.js"
-import userRoutes from "./routes/user.routes.js"
-import postRoutes from "./routes/post.routes.js"
+import authRoutes from "./routes/auth.route.js"
+import userRoutes from "./routes/user.route.js"
+import postRoutes from "./routes/post.route.js"
 import cookieParser from "cookie-parser"
 
 dotenv.config()
